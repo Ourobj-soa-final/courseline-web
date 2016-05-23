64e6c2941e10018ad74dcc73b331bf9ca68f3bb7
Smallpath <smallpath2013@gmail.com>
2016-05-23 18:27:02 +0800

    初始化"/users"的路由

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Get method');
 });
+
+router.post('/', function(req, res, next) {
+  res.send('Post method');
+});
+
+router.put('/', function(req, res, next) {
+  res.send('Put method');
+});
+
+router.delete('/', function(req, res, next) {
+  res.send('Delete method');
+});
+
+
 
 module.exports = router;
