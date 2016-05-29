f13c0f741903bc7af05a229e901b7ce66d97ed38
Smallpath <smallpath2013@gmail.com>
2016-05-29 19:39:41 +0800

    [Update] 修改API名称

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Courseline Official API' });
+  res.render('index', { title: 'Courseline Official API v1.0' });
 });
 
 module.exports = router;
